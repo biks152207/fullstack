119635558ca93764d7dd769e4d4799524fdcbf5a
bikram-mits <bikram@makeitsimple.info>
2015-11-08 22:03:33 +0545

    changes

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://biks12:beatles1@ds043338.mongolab.com:43338/bikram'
+    uri: 'mongodb://beeks:beatles1@ds043338.mongolab.com:43338/bikram'
   },
 
   seedDB: true
