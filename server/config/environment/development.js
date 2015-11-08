07b737edd5b2576ba3735226ecf38542269abff6
bikram-mits <bikram@makeitsimple.info>
2015-11-08 19:30:07 +0545

    added password

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/trello-dev'
+    uri: 'mongodb://biks12:beatles1@ds043338.mongolab.com:43338/bikram'
   },
 
   seedDB: true
